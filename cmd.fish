0cebcae22315d030d26f45758fa2a1c24c2fcaa7
juliancoffee <lightdarkdaughter@gmail.com>
2023-09-27 15:41:49 +0300

    Init

---
+++ cmd.fish
@@ -0,0 +1,24 @@
+function trace
+    echo $argv
+    eval $argv
+end
+
+function buildgen
+    if not test -d build
+        echo "creating build directory"
+        trace mkdir build
+    end
+
+    trace cd build
+    trace cmake ..
+    trace cd ..
+end
+
+function remake
+    trace cd build
+    trace make
+    trace cd ..
+end
+
+echo "added remake command"
+echo "added buildgen command"

f1ea5c23a64a8b2163b00b031fb611b5d8aa45e3
juliancoffee <lightdarkdaughter@gmail.com>
2023-09-27 18:02:41 +0300

    Add timing utilities

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 #include <fmt/core.h>
+#include "utils.hpp"
 
-int main() {
-    fmt::print("Hello\n");
+int fib(size_t n) {
+    if (n < 3) {
+        return 1;
+    }
+
+    return fib(n-1) + fib(n-2);
+}
+
+auto main() -> int {
+    auto timed = timeit("fib", []() {
+            return fib(30);
+    });
+    fmt::print("Hello. Fib(30) is {}.\n", timed.res());
+    timed.show();
 }
